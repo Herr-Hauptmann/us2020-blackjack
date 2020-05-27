6973fae0e4fee5e7b0d3771e08465ce748ff01ea
_Hauptmann_ <tarik.horoz@gmail.com>
2020-05-27 19:19:40 +0200

    second commit

---
+++ [US] Blackjack.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <cstdlib>
+
+enum Znak {Karo, Pik, Herc, Tref}; //baklava, list, srce, djetelina
+
+struct Karta
+{
+	char vrijednost;
+	Znak znak;
+};
+
+void PopuniSpil(std::vector<Karta> spil)
+{
+	Znak znak;
+	for (int i = 0; i < 4; i++)
+	{
+		
+	}
+}
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
