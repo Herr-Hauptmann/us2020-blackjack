cc0fffb86fe34e14be57c67e010ce27adf239f66
_Hauptmann_ <tarik.horoz@gmail.com>
2020-05-27 19:49:39 +0200

    zapocet rad na klasi spil

--- [US] Blackjack.cpp
+++ [US] Blackjack.cpp
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <cstdlib>
 
-enum Znak {Karo, Pik, Herc, Tref}; //baklava, list, srce, djetelina
+enum Znak {Karo = 0, Pik = 1, Herc = 2, Tref = 3}; //baklava, list, srce, djetelina
 
 struct Karta
 {
@@ -11,13 +11,54 @@ struct Karta
 	Znak znak;
 };
 
-void PopuniSpil(std::vector<Karta> spil)
+class Spil 
+{
+	std::vector<Karta> spil;
+	
+public:
+	void popuniSpil();
+	Karta vuci();
+};
+Znak dajVrijednost(int a)
+{
+	switch (a)
+	{
+	case 0: return Karo; break;
+	case 1: return Pik; break;
+	case 2: return Herc; break;
+	case 3: return Tref; break;
+	default:
+		throw std::logic_error("Nevazeci broj");
+	}
+}
+
+void Spil::popuniSpil()
 {
 	Znak znak;
 	for (int i = 0; i < 4; i++)
 	{
-		
+		znak = dajVrijednost(i);
+		for (int i = 1; i <= 13; i++)
+		{
+			char vrijednost;
+			if (i == 1)
+				vrijednost = 'A';
+			else if (i == 11)
+				vrijednost = 'B';
+			else if (i == 12)
+				vrijednost = 'Q';
+			else if (i == 13)
+				vrijednost = 'K';
+			else 
+				vrijednost = '0'+ i;
+			spil.push_back({ vrijednost, znak });
+		}
 	}
+}
+
+Karta Spil::vuci()
+{
+
 }
 
 int main()
